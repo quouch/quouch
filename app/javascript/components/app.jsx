11ccb67d573f1a97f30db191a662e9d732742424
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2022-10-20 16:21:09 +0200

    vite and react setup

---
+++ app/javascript/components/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import header from '../../assets/images/header.jpg';
+
+const App = () => {
+  return (
+    <div>
+      <img src={header} className=""></img>
+    </div>
+  );
+};
+
+export default App;
