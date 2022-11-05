7916586f252b587a4ce9b73ac5736c6926295af2
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2022-11-05 12:55:50 +0100

    add number of travellers column to bookings table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_01_110133) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_05_073756) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,6 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_01_110133) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "donation_amount"
+    t.integer "number_travellers"
     t.index ["couch_id"], name: "index_bookings_on_couch_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
