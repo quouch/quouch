0e5f8cd1a738139fdb1849bfda0b8ed5514e0ad4
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2022-11-05 12:53:14 +0100

    add age validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,10 +18,31 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :pronouns, presence: true, inclusion: { in: ['she/her', 'he/him', 'they/them'] }
   validates :date_of_birth, presence: true
+  validate :validate_age
   validates :summary, presence: true, length: { minimum: 100, too_short:
     "We want to know more about you - write at least 100 characters about yourself" }
   validates :question_one, presence: true, length: { minimum: 100, too_short: "Write at least 100 characters" }
   validates :question_two, presence: true, length: { minimum: 100, too_short: "Write at least 100 characters" }
   validates :question_three, presence: true, length: { minimum: 100, too_short: "Write at least 100 characters" }
   validates :question_four, presence: true, length: { minimum: 100, too_short: "Write at least 100 characters" }
+
+  def calculated_age
+    today = Date.today
+    if today.month > date_of_birth.month || (today.month == date_of_birth.month && today.day >= date_of_birth.day)
+      calculation = 0
+    else
+      calculation = 1
+    end
+    today.year - date_of_birth.year - calculation
+  end
+
+  def validate_age
+    if calculated_age.present? && calculated_age > 18.years.ago.to_i
+      errors.add(:birth_date, 'You should be over 18 years old.')
+    end
+  end
+
+  def full_name
+    return first_name + last_name
+  end
 end
