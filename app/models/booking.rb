3d5a00c138a7c1111eb2003b7f873a4381ac224d
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2022-11-09 21:37:27 +0100

    add booking save method, sent method + view, show method + view, routes and error messages

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,6 +6,21 @@ class Booking < ApplicationRecord
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :donation_amount, numericality: true, allow_nil: true
+  validate :date_in_future?
+  validate :valid_dates?
+
 
   enum status: { pending: 0, confirmed: 1, completed: 2, cancelled: -1 }
+
+  def date_in_future?
+    if Date.yesterday > start_date
+      errors.add(:start_date, "Booking can't be in the past")
+    end
+  end
+
+  def valid_dates?
+    if start_date > end_date
+      errors.add(:start_date, "Dates not valid")
+    end
+  end
 end
