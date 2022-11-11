2adc15db7a4a2a8a0de2ab7752d3542feaaf0325
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2022-11-11 19:24:38 +0100

    move from vite.config.js to .ts, update manifest.js, import components through application.scss, install dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import FullReload from 'vite-plugin-full-reload'
+// import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
-    FullReload(['config/routes.rb', 'app/views/**/*'], { delay: 200 })
+    FullReload(['config/routes.rb', 'app/views/**/*'], { delay: 200 }),
+    // react()
   ],
 })
