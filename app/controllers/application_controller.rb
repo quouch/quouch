ff0e26979e2a5330f2a3455c0c939fb79d0a4c33
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2022-11-18 15:38:09 +0100

    add missing columns to edit registration view, add permitted params, display on couch show view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :date_of_birth, :email, :password, :password_confirmation, ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :date_of_birth, :email, :password, :password_confirmation])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, couch_attributes: [:capacity]])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:photo, :first_name, :last_name, :date_of_birth, :pronouns, :city_id, :summary, :offers_couch, :offers_co_work, :offers_hang_out, :question_one, :question_two, :question_three, :question_four, couch_attributes: [:capacity]])
   end
 end
