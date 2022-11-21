77701b996cf0d12155c13b8f4e06c1f1c3d62192
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2022-11-21 16:14:20 +0100

    implementation couch facility logic

--- app/models/couch.rb
+++ app/models/couch.rb
@@ -2,7 +2,8 @@ class Couch < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :reviews, dependent: :destroy
-  has_many :couch_facilities
+  has_many :couch_facilities, dependent: :destroy, autosave: true
+  accepts_nested_attributes_for :couch_facilities
   has_many :facilities, through: :couch_facilities
 
   validates :capacity, inclusion: { in: [1, 2, 3, 4, 5, 6] }
