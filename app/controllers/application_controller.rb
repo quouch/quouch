2c37b0b4a1e6600dd35e1d3f6080a25864e93c0d
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2022-11-22 18:19:59 +0100

    permit all couch facility ids instead of singular

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :date_of_birth, :email, :password, :password_confirmation])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:photo, :first_name, :last_name, :date_of_birth, :pronouns, :city_id, :summary, :offers_couch, :offers_co_work, :offers_hang_out, :question_one, :question_two, :question_three, :question_four, couch_attributes: [:capacity], couch_facility_attributes: [:facility_id]])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:photo, :first_name, :last_name, :date_of_birth, :pronouns, :city_id, :summary, :offers_couch, :offers_co_work, :offers_hang_out, :question_one, :question_two, :question_three, :question_four, couch_attributes: [:capacity], couch_facility_attributes: [:facility_ids]])
   end
 end
