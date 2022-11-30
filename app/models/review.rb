b1ed6791f7fa196a8e09e2e1a82b0cb44cf95e82
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2022-11-30 14:53:48 +0100

    add new and create method to reviews controller, add new view for reviews, bugfix in case statement in booking index / request index

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,7 +3,6 @@ class Review < ApplicationRecord
   belongs_to :couch
   belongs_to :user
 
-  validates :description, presence: true, length: { minimum: 100, too_short:
-    "We would love to hear more details about your stay - please write at least 100 characters" }
-  validates :rating, presence: true, inclusion: { in: ['1', '2', '3', '4', '5'] }
+  validates :description, presence: true
+  validates :rating, presence: true, inclusion: { in: [1, 2, 3, 4, 5] }
 end
