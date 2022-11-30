fff200c133a64ba4da1a5b8d9b655d00a3f5751c
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2022-11-30 14:53:48 +0100

    add new and create method to reviews controller, add new view for reviews, bugfix in case statement in booking index / request index

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,9 +6,22 @@ class ReviewsController < ApplicationController
 	end
 
 	def new
+		@host = @couch.user
+		@review = Review.new
 	end
 
 	def create
+		@review = Review.new(review_params)
+		@review.couch = @couch
+		@review.booking = @booking
+		@review.user = current_user
+		if @review.save && @review.user == @booking.user
+			redirect_to bookings_path
+		elsif @review.save && @review.user == @booking.couch.user
+			redirect_to requests_couch_bookings_path
+		else
+			render new_couch_booking_review_path(@couch, @booking), notice: "Your review did not save, please try again"
+		end
 	end
 
 	private
@@ -24,4 +37,5 @@ class ReviewsController < ApplicationController
 	def set_couch
 		@couch = Couch.find(params[:couch_id])
 	end
+
 end
