7a79aaccdfdecda1a45b6a789a42712c0b5190ee
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2022-12-12 16:33:54 +0100

    adapt naming of react components, add chat channel spec file

--- app/javascript/entrypoints/index.jsx
+++ app/javascript/entrypoints/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from '../components/app.jsx';
+import App from '../components/App.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
