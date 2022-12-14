db039ed50580a3a9d54e7e4ac77eba5e255571c7
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2022-12-14 17:06:07 +0100

    move channels directory into right place

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -4,12 +4,6 @@ class ChatChannel < ApplicationCable::Channel
     stream_for chat
   end
 
-  def speak(data)
-    message = Message.create(body: data['message'])
-    socket = { message: message.body }
-    ChatChannel.broadcast_to('chat_channel', socket)
-  end
-
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
