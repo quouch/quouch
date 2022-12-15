be6f5653d570e5795ed98677a13e074d946a1961
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2022-12-15 12:15:55 +0100

    build stimulus controller, chats show page, set up routes for chats and messages, build message partial

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,22 @@
 class MessagesController < ApplicationController
-	def show
-		@message = Message.find(params[:id])
-		@chat = @message.chat
-	end
+  skip_before_action :verify_authenticity_token
+
+	def create
+    @chat = Chat.find(params[:chat_id])
+    @message = Message.new(message_params)
+    @message.chat = @chat
+    @message.user = current_user
+    @message.save
+    ChatChannel.broadcast_to(
+      @chat,
+      render_to_string(partial: "messages/message", locals: {message: @message})
+    )
+    # head :ok
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end 
