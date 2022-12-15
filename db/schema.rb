4ef40099b3339f3733552064a0ad81e47451fe8b
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2022-12-15 12:17:07 +0100

    change chat table for private messaging

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_13_143318) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_15_110553) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,9 +62,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_13_143318) do
   end
 
   create_table "chats", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_sender_id"
+    t.bigint "user_receiver_id"
+    t.index ["user_receiver_id"], name: "index_chats_on_user_receiver_id"
+    t.index ["user_sender_id"], name: "index_chats_on_user_sender_id"
   end
 
   create_table "cities", force: :cascade do |t|
@@ -161,6 +164,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_13_143318) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "couches"
   add_foreign_key "bookings", "users"
+  add_foreign_key "chats", "users", column: "user_receiver_id"
+  add_foreign_key "chats", "users", column: "user_sender_id"
   add_foreign_key "cities", "countries"
   add_foreign_key "couch_facilities", "couches"
   add_foreign_key "couch_facilities", "facilities"
