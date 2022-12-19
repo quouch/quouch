cce43aecd4d397a223fb592b4a2907d804839feb
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2022-12-19 15:59:56 +0100

    setup stripe in setup mode

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,5 +1,6 @@
 class PaymentsController < ApplicationController
 	def new
     @booking = current_user.bookings.where(status: 'pending').find(params[:booking_id])
+    @nights = (@booking.end_date - @booking.start_date).to_i
   end
 end
