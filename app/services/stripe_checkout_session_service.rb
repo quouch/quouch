b071a5b8921cd021d515e616468bffe8c2f63ebf
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2022-12-22 18:26:39 +0100

    bug fix: call method in stripe checkout session now using where instead of find_by

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -1,6 +1,6 @@
 class StripeCheckoutSessionService
   def call(event)
-    booking = Booking.find_by(checkout_session_id: event.data.object.id)
+    booking = Booking.where(checkout_session_id: event.data.object.id)
     booking.update(payment_status: 1)
   end
 end
