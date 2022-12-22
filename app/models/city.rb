61ecff18002afc7eb9163bf3ff7041125d7d04fe
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2022-12-23 00:07:59 +0100

    add pg search for city model

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,4 +1,11 @@
 class City < ApplicationRecord
+  include PgSearch::Model
+  pg_search_scope :search_by_name,
+    against: [ :name ],
+    using: {
+      tsearch: { prefix: true }
+    }
+  
   belongs_to :country
   has_many :users
 
