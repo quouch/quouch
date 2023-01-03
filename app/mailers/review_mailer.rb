c2f4beb2349bc3e0af7f6f82dfbb4f61a2a4dc5b
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2023-01-03 12:30:21 +0100

    generate mailers, add welcome email, add first skeleton

---
+++ app/mailers/review_mailer.rb
@@ -0,0 +1,9 @@
+class ReviewMailer < ApplicationMailer
+	default from: 'dev.quouch@gmail.com'
+
+	def new_review_email
+	end
+
+	def add_review_email
+	end
+end
---
+++ spec/mailers/previews/review_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/review
+class ReviewPreview < ActionMailer::Preview
+
+end
