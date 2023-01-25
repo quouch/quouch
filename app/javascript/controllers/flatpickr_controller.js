24ec752c927d70c4a62523650fec72f1b58acf70
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2023-01-25 23:23:05 +0100

    add flatpickr for booking form

---
+++ app/javascript/controllers/flatpickr_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus"
+import flatpickr from "flatpickr";
+
+// Connects to data-controller="flatpickr"
+export default class extends Controller {
+  static targets = [ "startTime", "endTime" ]
+
+  connect() {
+    flatpickr(this.startTimeTarget, {
+      altInput: true
+    })
+    flatpickr(this.endTimeTarget, {
+      altInput: true
+    })
+  }
+}
