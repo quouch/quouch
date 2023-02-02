9e33570c46f6a376a157b413e10bf47a7b0e7b6f
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2023-02-02 17:02:54 +0100

    add star rating library, add to review form, style review form, improve booking and request styles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_02_112601) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_02_143841) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -148,7 +148,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_02_112601) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.string "content"
+    t.text "content"
     t.integer "rating"
     t.bigint "booking_id", null: false
     t.bigint "couch_id", null: false
