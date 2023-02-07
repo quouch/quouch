325ce9fe8ac118fc5862244015d3d76dfc8de64b
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2023-02-07 11:42:28 +0100

    remove city show, build member route for city couches, add styles for couches index pages

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -8,4 +8,15 @@ class CitiesController < ApplicationController
 		@city_id = params[:id]
 		@hosts = User.where(city_id: @city_id)
 	end
+	
+	def couches
+		@city = City.find(params[:id])
+		@users = User.where(city: @city)
+		@couches = []
+
+		@users.each do |user| 
+			@couch = user.couch
+			@couches << @couch
+		end
+	end
 end
