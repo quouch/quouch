352bdeef1ca0b3920bde47ec154d7f0e77446a11
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2023-02-13 20:57:49 +0100

    add geocoder gem, rename vite.config.ts to .js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import FullReload from 'vite-plugin-full-reload'
-// import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
