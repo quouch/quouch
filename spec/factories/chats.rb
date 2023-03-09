6630399a9aad211a0c17edf7513b5c40ca62c63f
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2023-03-09 20:14:43 +0100

    remove rspec files

--- spec/factories/chats.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :chat do
-    name { "MyString" }
-  end
-end
--- spec/factories/notifications.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :notification do
-    recipient { nil }
-    type { "" }
-    params { "" }
-    read_at { "2023-01-05 13:37:48" }
-  end
-end
