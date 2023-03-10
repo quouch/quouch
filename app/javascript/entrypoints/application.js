5c334698886d2f8adccc0555c53392116a9ce838
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2023-03-10 18:42:20 +0100

    add JS + SCSS entrypoints, add logic to invitations edit action, code clean up and refactoring, add error messages in sign up form

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -1,5 +1,7 @@
 import { Application } from "@hotwired/stimulus"
 
+import '../../assets/stylesheets/application.scss';
+
 const application = Application.start()
 
 // Configure Stimulus development experience
