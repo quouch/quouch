0c108bb7d477c06322dd648739c6c306e3eb14e9
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2023-03-30 23:19:50 +0200

    fix seeds, add svgs from cloudinary to booking form

--- db/migrate/20221216111919_remove_amount_columns.rb
+++ db/migrate/20221216111919_remove_amount_columns.rb
@@ -1,6 +1,5 @@
 class RemoveAmountColumns < ActiveRecord::Migration[7.0]
   def change
-    remove_column :bookings, :minimum_amount
     remove_column :bookings, :donation_amount
     remove_column :bookings, :total_amount
   end
