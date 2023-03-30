07b373dc51557473d8088347f40f582b84344f45
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2023-03-30 22:03:18 +0200

    implement chat notifications and message notification mail

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,5 +1,7 @@
 class Chat < ApplicationRecord
 	has_many :messages
 
+	has_noticed_notifications model_name: 'Notification'
+
 	validates :user_sender_id, uniqueness: { scope: :user_receiver_id }
 end
