faeb4a212c2b519be7468ca4817d148a24488b4c
claire demont <claire.edith.demont@gmail.com>
2023-04-12 18:25:21 +0200

    search working and remove all bad occurences of city and coutry as instances
    Signed-off-by: claire demont <claire.edith.demont@gmail.com>

--- app/models/characteristic.rb
+++ app/models/characteristic.rb
@@ -1,4 +1,5 @@
 class Characteristic < ApplicationRecord
-	has_many :user_characteristics, dependent: :destroy
-	has_many :users, through: :user_characteristics
+  has_many :user_characteristics, dependent: :destroy
+  has_many :users, through: :user_characteristics
+  has_many :couches, through: :users
 end
--- app/models/couch.rb
+++ app/models/couch.rb
@@ -2,26 +2,23 @@ class Couch < ApplicationRecord
   include PgSearch::Model
 
   belongs_to :user
-  has_one    :city, through: :user
-  has_one    :country, through: :user
+  has_many   :user_characteristics, through: :user
+  has_many   :characteristics, through: :user_characteristics
   has_many   :bookings, dependent: :destroy
   has_many   :reviews, dependent: :destroy
   has_many   :couch_facilities, dependent: :destroy, autosave: true
   has_many   :facilities, through: :couch_facilities
   accepts_nested_attributes_for :couch_facilities
 
-  pg_search_scope :search, associated_against: {
-    city: :name,
-    country: :name,
-    using: {
-      tsearch: { prefix: true }
-    },
-    associated_against: {
-      user: {
-        user_characteristics: {
-          characteristic: :name
-        }
-      }
-    }
-  }
+  pg_search_scope :search,
+                  associated_against: {
+                    user: [
+                      :address,
+                      :city,
+                      :country
+                    ]
+                  },
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 end
