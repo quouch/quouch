59cad885e433ff95d13a1c92d281baa9fcc971ae
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2023-04-14 16:47:38 +0200

    fix render after invite code validation failed

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -9,7 +9,7 @@ class InvitesController < ApplicationController
 			redirect_to new_user_registration_path(invite_code:)
 		else
 			flash[:alert] = 'Invite code not valid. Try again or contact the Quouch team'
-			render :action
+			render :invite_code_form, status: :unprocessable_entity
 		end
 	end
 end
