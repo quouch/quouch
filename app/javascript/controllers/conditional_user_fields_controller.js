1541fc7b89072374667259a04361b480bfdb6aff
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2023-04-14 10:39:56 +0200

    hide offer fields if user chooses travelling, add validation to choose at least one

--- app/javascript/controllers/conditional_user_fields_controller.js
+++ app/javascript/controllers/conditional_user_fields_controller.js
@@ -1,14 +1,14 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus'
 
 // Connects to data-controller="conditional-user-fields"
 export default class extends Controller {
-  static targets = ["trigger", "field"]
+  static targets = ['trigger', 'field']
 
    toggleField() {
     if (this.triggerTarget.checked) {
-      this.fieldTarget.style.display = "flex"
+      this.fieldTarget.style.display = 'flex'
     } else {
-      this.fieldTarget.style.display = "none"
+      this.fieldTarget.style.display = 'none'
     }
   }
 }
--- app/javascript/controllers/display_info_window_controller.js
+++ app/javascript/controllers/display_info_window_controller.js
@@ -2,15 +2,18 @@ import { Controller } from "@hotwired/stimulus"
 import swal from 'sweetalert'
 
 export default class extends Controller {
-  static targets = ['checkbox']
+  static targets = ['checkbox', 'list']
 
   displayInfo() {
     if (this.checkboxTarget.checked) {
+      this.listTarget.style.display = 'none'
       swal({
         title: 'Info',
         text: "If you select that you are currently travelling, your profile will show only the travelling badge and you won't appear as a host.",
         button: 'Ok, I understand!'
       })
+    } else {
+      this.listTarget.style.display = 'block'
     }
   }
 }
