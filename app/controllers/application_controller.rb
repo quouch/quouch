2a52261dafb1cce0d84c3181fbbc7c45e1383310
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2023-04-17 11:57:01 +0200

    update new registrations form and application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/users/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:accept_invitation, keys: [:photo, :first_name, :last_name, :date_of_birth,
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:photo, :first_name, :last_name, :date_of_birth,
                                       :pronouns, :address, :street, :zipcode, :city, :country, :summary, :travelling,
                                       :offers_couch, :offers_co_work, :offers_hang_out, :question_one, :question_two,
                                       :question_three, :question_four, :address,
