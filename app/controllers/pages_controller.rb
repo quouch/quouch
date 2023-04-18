aa6a9d34c4276f5b733fbcc73de3d5885808e316
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2023-04-18 15:29:12 +0200

    about page accessible without login

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[home]
+  skip_before_action :authenticate_user!, only: %i[home about]
 
   def home
     @couches = Couch.where.not(user: current_user)
