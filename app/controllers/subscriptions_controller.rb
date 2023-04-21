1100266a50d595d68961611261bc3e6ddfe2a296
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2023-04-21 22:28:08 +0200

    added collections column to subscriptions table, added plans view/subscription new

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,25 +1,34 @@
 class SubscriptionsController < ApplicationController
-  before_action :set_subscription, except: %i[create]
+  before_action :set_subscription, except: %i[new create]
+
+  def new
+    @plans = Plan.all
+    @supporter = get_plans(@plans, 'supporter')
+    @member = get_plans(@plans, 'member')
+    @community = get_plans(@plans, 'community')
+
+  end
 
   def show
-    render json: @subscription
+    @subscription
   end
 
   def create
     @subscription = Subscription.new(subscription_params)
 
     if @subscription.save
-      render json: @subscription, status: :created
+      render 'subscription/show', status: :created
     else
-      render json: @subscription.errors, status: :unprocessable_entity
+      render 'subscription/new', status: :unprocessable_entity
     end
   end
 
   def update
     if @subscription.update(subscription_params)
-      render json: @subscription
+      render 'subscription/show', status: :created
     else
-      render json: @subscription.errors, status: :unprocessable_entity
+      flash[:alert] = 'Something went wrong. Please try again or contact the Quouch Team.'
+      render 'subscription/show', status: :unprocessable_entity
     end
   end
 
@@ -31,5 +40,9 @@ class SubscriptionsController < ApplicationController
 
   def subscription_params
     params.require(:data).permit(:card_number, :exp_month, :exp_year, :cvc, :user_id, :plan_id, :active)
+  end
+
+  def get_plans(plans, collection)
+    plans.where(collection:).order('id')
   end
 end
