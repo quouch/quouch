f84d7c1a7501dbfbf07cedc6e7f73e9baac49cc5
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2023-05-26 08:52:58 +0300

    update validations for subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,20 +2,36 @@ class Subscription < ApplicationRecord
   belongs_to :plan
   belongs_to :user
 
-  validates :stripe_id, presence: true, uniqueness: true
+  # validates :stripe_id, presence: true, uniqueness: true
+  validate :validate_single_subscription
 
-  before_validation :create_stripe_reference, on: :create
+  # after_validation :create_stripe_reference, on: :create
+  # before_validation :update_stripe_reference, on: :update
   before_update     :cancel_stripe_subscription, if: :subscription_inactive?
 
   def create_stripe_reference
     response = Stripe::Subscription.create({
-      customer: self.user.stripe_id,
+      customer: user.stripe_id,
       items: [
-        { price: self.plan.stripe_price_id }
+        { price: plan.stripe_price_id }
       ],
       payment_behavior: 'default_incomplete',
-      payment_settings: {save_default_payment_method: 'on_subscription'}
+      payment_settings: { save_default_payment_method: 'on_subscription' }
     })
+
+    self.stripe_id = response.id
+  end
+
+  def update_stripe_reference
+    response = Stripe::Subscription.update(
+      stripe_id,
+      {
+        items: [
+          { price: plan.stripe_price_id }
+        ]
+      },
+      subscription: stripe_id
+    )
 
     self.stripe_id = response.id
   end
@@ -26,5 +42,9 @@ class Subscription < ApplicationRecord
 
   def subscription_inactive?
     !active
+  end
+
+  def validate_single_subscription
+    nil?
   end
 end
