5431913001ad87ac5028b4a6cdefd57d48ac14e8
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2023-06-26 16:20:05 +0200

    update couches queries to improve performance and load in batches

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
 
   def home
     @couches = Couch.where.not(user: current_user)
-    @active_couches = @couches.includes(:reviews, user: [{ photo_attachment: :blob }, :characteristics])
+    @active_couches = @couches.includes(:reviews, user: [{ photo_attachment: :blob }, :characteristics]).find_each(batch_size: 100)
   end
 
   def search_cities
