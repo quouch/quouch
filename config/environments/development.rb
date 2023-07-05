b5934d624fe2c90ab2fc3d31f28ce092f8606a03
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2023-07-05 12:38:17 +0200

    add gmail credentials to credentials file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,13 +43,13 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
-    :address              => 'smtp.gmail.com',
-    :port                 => 587,
-    :domain               => 'gmail.com',
-    :authentication       => :plain,
-    :user_name            => ENV['GMAIL_USERNAME'],
-    :password             => ENV['GMAIL_PASSWORD'],
-    :enable_starttls_auto => true
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: Rails.application.credentials.gmail.address,
+    password: Rails.application.credentials.gmail.password,
+    authentication: 'plain',
+    enable_starttls_auto: true
   }
 
   config.action_mailer.perform_caching = false
