52cf16e041c20fb4a99a0161948900ee8ba40070
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2023-07-28 14:20:03 +0200

    require dotenv in application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
-require 'dotenv/rails-now'
+require 'dotenv/rails'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
