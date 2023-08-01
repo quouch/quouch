3deef4c941ed882f41450a1d57005eaf35325fa0
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2023-08-01 14:21:24 +0200

    add file size limitation pop up for profile picture, update labels and fields in sign up form

--- app/javascript/controllers/image_preview_controller.js
+++ app/javascript/controllers/image_preview_controller.js
@@ -3,8 +3,17 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ['input', 'preview']
 
-  previewImage() {
-    const url = URL.createObjectURL(this.inputTarget.files[0])
-    this.previewTarget.src = url
+  previewImage(event) {
+    const file = event.target.files[0]
+    const maxSize = 10485760 // 10MB in bytes
+
+    if (file && file.size > maxSize) {
+      alert('File size must be less than 10MB so we can keep the site fast!')
+      event.preventDefault()
+      event.target.value = ''// Reset the input field to clear the invalid file
+    } else {
+      const url = URL.createObjectURL(this.inputTarget.files[0])
+      this.previewTarget.src = url
+    }
   }
 }
