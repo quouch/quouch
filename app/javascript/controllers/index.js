7d353b13a9dde10a0ffb650b57dd37fc21dbbd83
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2023-08-01 22:01:36 +0200

    bug fix: bring back stimulus autocomplete

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import { Autocomplete } from 'stimulus-autocomplete'
+application.register('autocomplete', Autocomplete)
+
 import AddressAutocompleteController from "./address_autocomplete_controller"
 application.register("address-autocomplete", AddressAutocompleteController)
 
