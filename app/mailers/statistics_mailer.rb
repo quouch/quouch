78be49b80ca7ac8cd1f27443aa14751eaf7309fc
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2023-09-02 18:20:08 +0200

    improve stats report

--- app/mailers/statistics_mailer.rb
+++ app/mailers/statistics_mailer.rb
@@ -3,7 +3,14 @@ class StatisticsMailer < ApplicationMailer
 		# Collect your stats here
 		@total_users = User.count
 		@users_per_city = users_per_city
+		@users_per_country = users_per_country
+		@amount_reviews = Review.count
+		@review_average = Review.average(:rating)
+		@reviews_today = Review.where(created_at: Date.today)
 		@bookings_today = bookings_today
+		@completed_bookings = Booking.where(status: 4)
+		@confirmed_bookings = Booking.where(status: 1)
+		@cancelled_bookings = Booking.where(status: -1)
 
 		mail(to: 'nora@quouch-app.com', subject: 'User Stats Report')
 	end
@@ -12,6 +19,11 @@ class StatisticsMailer < ApplicationMailer
 
 	def users_per_city
 		result = User.all.group_by(&:city)
+		result.transform_values(&:count)
+	end
+
+	def users_per_country
+		result = User.all.group_by(&:country)
 		result.transform_values(&:count)
 	end
 
