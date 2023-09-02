0a038cc329391aa11d4bb121f6d1182cbf4c54cd
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2023-09-02 17:51:34 +0200

    remove reset password token error and add full error messages

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -55,7 +55,7 @@ SimpleForm.setup do |config|
     # b.use :input, class: 'input', error_class: 'is-invalid', valid_class: 'is-valid'
     b.use :label_input
     b.use :hint,  wrap_with: { tag: :span, class: :hint }
-    b.use :error, wrap_with: { tag: :span, class: :error }
+    b.use :error, wrap_with: { tag: :span, class: :form_error }
 
     ## full_messages_for
     # If you want to display the full error message for the attribute, you can
