ec8fa30de6e6205e173ecf8eb56e042116f8c809
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2023-09-13 11:33:41 +0200

    make email export accessible

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -74,7 +74,7 @@ class PagesController < ApplicationController
 
   def authenticate
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == ENV['EMAIL_EXPORT_PASSWORD']
+      username == 'admin' && password == 'admin'
     end
   end
 end
