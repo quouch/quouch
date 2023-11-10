74866ef62e12d6ea1b1e4eaec804672bbfb87ef5
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2023-11-10 12:28:18 +0200

    remove unused code in subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,15 +2,9 @@ class Subscription < ApplicationRecord
   belongs_to :plan
   belongs_to :user
 
-  # validates :stripe_id, presence: true, uniqueness: true
-
-  before_update :cancel_stripe_subscription, if: :subscription_inactive?
+  private
 
   def cancel_stripe_subscription
     Stripe::Subscription.cancel(stripe_id)
-  end
-
-  def subscription_inactive?
-    !active
   end
 end
