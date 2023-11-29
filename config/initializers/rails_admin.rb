3de1776470602d68e2fbfbdd2a7ee584acd2e6b5
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2023-11-29 13:09:02 +0200

    add configs for admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -4,10 +4,17 @@ RailsAdmin.config do |config|
   ### Popular gems integration
 
   ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+  end
+  config.current_user_method(&:current_user)
+
+  config.authorize_with do
+    unless current_user.admin?
+      flash[:alert] = 'Sorry, no admin access for you.'
+      redirect_to main_app.root_path
+    end
+  end
 
   ## == CancanCan ==
   # config.authorize_with :cancancan
