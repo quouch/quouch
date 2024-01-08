fda48815ff8184293e6cea3b12296a755c627e94
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2024-01-09 00:00:01 +0200

    update credentials and environment variables for production

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,7 +3,7 @@ require_relative '../../app/services/stripe_checkout_session_service'
 Rails.configuration.stripe = {
   publishable_key: ENV.fetch('STRIPE_PUBLISHABLE_KEY'),
   secret_key:      ENV.fetch('STRIPE_SECRET_KEY'),
-  signing_secret:  ENV.fetch('STRIPE_WEBHOOK_SECRET_KEY')
+  signing_secret:  ENV.fetch('STRIPE_SIGNING_SECRET_KEY')
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
