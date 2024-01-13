f248e2def6cfb11eea925dac714ef349bdb48f84
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2024-01-14 00:51:01 +0530

    add brevo controller, add fetch request to create contact in mailing list list

---
+++ app/javascript/controllers/brevo_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="brevo"
+export default class extends Controller {
+  static targets = ['input']
+  static values = { apiKey: String }
+
+  send(event) {
+    event.preventDefault();
+
+    fetch('https://api.brevo.com/v3/contacts', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json', 'api-key': this.apiKeyValue },
+      body: JSON.stringify({ 'email': this.inputTarget.value, 'listIds': [2] })
+    })
+      .then(response => response.json())
+  }
+}
