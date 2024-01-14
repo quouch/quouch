5b3c83aec0a628a5424f426baea4762f8100ceb1
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2024-01-14 22:17:15 +0530

    console log response

--- app/javascript/controllers/brevo_controller.js
+++ app/javascript/controllers/brevo_controller.js
@@ -16,15 +16,17 @@ export default class extends Controller {
       body: JSON.stringify({ 'email': this.inputTarget.value, 'listIds': [2] })
     })
       .then((response) => {
-        if (response.status === 200) {
-          swal({
-            title: 'Signed up!',
-            text: 'You have successfully subscribed to our newsletter and will now stay up to date. We would love to welcome you into our community!',
-            button: 'Lots of love!'
-          }).then(() => {
-            window.location.reload()
-          })
-        }
+        console.log(response)
       })
+      //   if (response.status === 200) {
+      //     swal({
+      //       title: 'Signed up!',
+      //       text: 'You have successfully subscribed to our newsletter and will now stay up to date. We would love to welcome you into our community!',
+      //       button: 'Lots of love!'
+      //     }).then(() => {
+      //       window.location.reload()
+      //     })
+      //   }
+      // })
   }
 }
