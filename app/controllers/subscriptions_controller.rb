b494bcbf7b1c0c4216a75eb65451a6f77c337d07
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2024-01-17 17:32:53 +0530

    add paypal as payment method types

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -61,7 +61,7 @@ class SubscriptionsController < ApplicationController
     Stripe::Checkout::Session.create(
       {
         customer: current_user.stripe_id,
-        payment_method_types: ['card'],
+        payment_method_types: %w[card paypal],
         line_items: [{
           price: subscription.plan.stripe_price_id,
           quantity: 1
