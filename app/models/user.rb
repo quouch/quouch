a60ad673144a215f4555e47705c0806ad934f639
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2024-01-23 14:01:20 +0530

    bring back create stripe reference error logging

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ApplicationRecord
+class User < ApplicationRecord # rubocop:disable Metrics/ClassLength
   # Include default devise modules. Others available are:
   # :lockable, :trackable and :omniauthable
   include PgSearch::Model
@@ -108,6 +108,15 @@ class User < ApplicationRecord
   def create_stripe_reference
     response = Stripe::Customer.create(email:)
     self.stripe_id = response.id
+  rescue Stripe::StripeError => e
+    handle_stripe_reference_creation_error("Error creating Stripe customer: #{e.message}")
+  rescue StandardError => e
+    handle_stripe_reference_creation_error("An unexpected error occurred during Stripe customer creation: #{e.message}")
+  end
+
+  def handle_stripe_reference_creation_error(error_message)
+    flash[:error] = error_message
+    redirect_to new_subscription_url
   end
 
   def cancel_stripe_subscription
