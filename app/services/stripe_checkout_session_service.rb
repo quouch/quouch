a5d5aaf54879f42ff68335021b4b578c9786bf5c
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2024-03-04 19:10:06 +0200

    add emails, update terms

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -3,5 +3,6 @@ class StripeCheckoutSessionService
     checkout_session = event.data.object
     subscription = Subscription.find_by(checkout_session_id: checkout_session.id)
     subscription.update!(stripe_id: checkout_session.subscription)
+    SubscriptionMailer.with(subscription: @subscription).subscription_successful.deliver_later
   end
 end
