f058b06e042fc8e5d873f991cea0fb6adcf24556
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2024-03-04 20:58:43 +0200

    remove subscription clean_up class method

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,11 +1,4 @@
 class Subscription < ApplicationRecord
   belongs_to :plan
   belongs_to :user
-
-  def self.clean_up
-    incomplete_subscriptions = Subscription.where(stripe_id: nil)
-    return if incomplete_subscriptions.empty?
-
-    incomplete_subscriptions.destroy_all
-  end
 end
