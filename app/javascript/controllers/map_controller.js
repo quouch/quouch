e5679eb3eb13269604f8cc6d22ffdae5350437c0
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2024-03-08 17:04:54 +0200

    remove webpack &  yarn and npm and pin all libraries for importmap usage

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,4 +1,5 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus'
+import mapboxgl from 'mapbox-gl'
 
 // Connects to data-controller="map"
 export default class extends Controller {
