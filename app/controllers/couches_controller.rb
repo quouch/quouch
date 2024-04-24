d8ced5ebdee112567d18d05afd76ed8b707ed4ae
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2024-04-24 15:03:57 +0200

    clean up filter logic from pages controller into couches controller

--- app/controllers/couches_controller.rb
+++ app/controllers/couches_controller.rb
@@ -1,18 +1,18 @@
 class CouchesController < ApplicationController
   def index
-    # repeating code from home controller as a quick performance fix
-    @active_couches = Couch.includes(:reviews, user: [{ photo_attachment: :blob }, :characteristics])
-                           .joins(:user)
-                           .where.not(user: current_user)
-                           .where.not(user: { first_name: nil })
-                           .where.not(user: { city: nil })
-                           .where.not(user: { country: nil })
+    @unfiltered_couches = Couch.includes(:reviews, user: [{ photo_attachment: :blob }, :characteristics])
+                               .joins(:user)
+                               .where.not(user: current_user)
+                               .where.not(user: { first_name: nil, city: nil, country: nil })
+
+    @shuffled_couches = @unfiltered_couches.shuffle
+    @couches = Kaminari.paginate_array(@shuffled_couches).page(params[:page]).per(9)
 
     apply_search_filter if params[:query].present?
     apply_characteristics_filter if params[:characteristics].present?
     apply_offers_filter if params.keys.any? { |key| key.include?('offers') }
 
-    @active_couches = @active_couches.page(params[:page]).per(30)
+    @couches = @unfiltered_couches.page(params[:page]).per(9)
 
     respond_to do |format|
       format.html
@@ -29,18 +29,32 @@ class CouchesController < ApplicationController
             Chat.find_by(user_sender_id: current_user.id, user_receiver_id: @host.id)
   end
 
+  def search_cities
+    query = params[:q].presence&.downcase
+    cities = []
+    countries = []
+
+    if query.present?
+      cities = User.search_city_or_country(query).pluck(:city).uniq.sort
+      countries = User.search_city_or_country(query).pluck(:country).uniq.sort
+    end
+
+    @results = (cities + countries).select { |entry| entry.downcase.starts_with?(query) }
+    render layout: false
+  end
+
   private
 
   def apply_search_filter
-    @active_couches = @active_couches.search(params[:query])
+    @unfiltered_couches = @unfiltered_couches.search(params[:query])
   end
 
   def apply_characteristics_filter
-    @active_couches = @active_couches.joins(user: { user_characteristics: :characteristic })
-                                     .where(characteristics: { id: params[:characteristics] })
-                                     .group('couches.id')
-                                     .having('COUNT(DISTINCT characteristics.id) = ?', params[:characteristics].length)
-                                     .reorder(nil)
+    @unfiltered_couches = @unfiltered_couches.joins(user: { user_characteristics: :characteristic })
+                                  .where(characteristics: { id: params[:characteristics] })
+                                  .group('couches.id')
+                                  .having('COUNT(DISTINCT characteristics.id) = ?', params[:characteristics].length)
+                                  .reorder(nil)
   end
 
   def apply_offers_filter
@@ -51,6 +65,6 @@ class CouchesController < ApplicationController
     offers_conditions[:offers_couch] = true if params[:offers_couch]
     offers_conditions[:travelling] = false
 
-    @active_couches = @active_couches.joins(:user).where(user: offers_conditions) if offers_conditions.any?
+    @unfiltered_couches = @unfiltered_couches.joins(:user).where(user: offers_conditions) if offers_conditions.any?
   end
 end
