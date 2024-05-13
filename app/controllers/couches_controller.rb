b0a7401ff9c6bf69a4162f3159f10cdfac48d11e
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2024-05-13 12:45:40 +0200

    remove .reorder call after applying characteristics filter

--- app/controllers/couches_controller.rb
+++ app/controllers/couches_controller.rb
@@ -54,7 +54,6 @@ class CouchesController < ApplicationController
                                          .where(characteristics: { id: params[:characteristics] })
                                          .group('couches.id')
                                          .having('COUNT(DISTINCT characteristics.id) = ?', params[:characteristics].length)
-                                         .reorder(nil)
   end
 
   def apply_offers_filter
