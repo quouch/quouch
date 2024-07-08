2fa98c3f84fcc11163747d20a00ce5a9c821be1f
papotte <berm.ross@gmail.com>
2024-07-08 17:40:50 +0200

    add tests for couch

--- test/models/couch_test.rb
+++ test/models/couch_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class CouchTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save couch without a user' do
+    couch = Couch.new
+    assert_not couch.valid?
+  end
+
+  test 'should save couch with a user' do
+    @user = FactoryBot.create(:test_user)
+
+    couch = Couch.new(user: @user)
+    assert couch.valid?
+  end
 end
