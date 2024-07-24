ca9eb1d86b6889b7f8bbe554c2ee055355b88319
papotte <berm.ross@gmail.com>
2024-07-24 13:35:44 +0200

    add routes tests

---
+++ test/integration/routes_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class RoutesTest < ActionDispatch::IntegrationTest
+  test "should route to api v1 login new" do
+    assert_routing '/api/v1/login', { format: :json, controller: "api/v1/users/sessions", action: "new" }
+  end
+
+  test "should route to api v1 login create" do
+    assert_routing({method: 'post', path: '/api/v1/login'}, {format: :json, controller: 'api/v1/users/sessions', action: 'create'})
+  end
+
+  test "should route to api v1 logout" do
+    assert_routing({ method: 'delete', path: '/api/v1/logout' }, { format: :json, controller: "api/v1/users/sessions", action: "destroy" })
+  end
+
+  test "should route to api v1 users edit" do
+    assert_routing 'api/v1/users/edit', { format: :json, controller: "api/v1/users/registrations", action: "edit" }
+  end
+
+  test "should route to api v1 update" do
+    id = "1"
+    assert_routing({ method: 'post', path: "api/v1/update/#{id}" }, { format: :json, controller: "api/v1/users/registrations", action: "update", id: id })
+  end
+end
