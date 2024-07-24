d8c601eea4470b0a208a07060ae4ea3093a2a21e
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2024-07-24 12:31:55 +0200

    fix: make icon for image upload placeholder img tag again for replacement functionality

--- app/javascript/controllers/image_preview_controller.js
+++ app/javascript/controllers/image_preview_controller.js
@@ -1,14 +1,14 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = ['input', 'preview']
+  static targets = ['input', 'preview', 'placeholder']
 
   previewImage(event) {
     const file = event.target.files[0]
-    const maxSize = 10485760 // 10MB in bytes
+    const maxSize = 5000000 // 5MB in bytes
 
     if (file && file.size > maxSize) {
-      alert('File size must be less than 10MB so we can keep the site fast!')
+      alert('File size must be less than 5MB')
       event.preventDefault()
       event.target.value = ''// Reset the input field to clear the invalid file
     } else {
