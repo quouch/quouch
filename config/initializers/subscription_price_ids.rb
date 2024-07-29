672bd463e62a7efbe489848e1c4354d5676d2f92
Lisbeth Purrucker <71633567+lisbethpurrucker@users.noreply.github.com>
2024-07-29 16:18:53 +0200

    expand stripe delete subscription service to create new subscription with new price id if subscription references old price id, add price id global variable as stripe config file (#66)

---
+++ config/initializers/subscription_price_ids.rb
@@ -0,0 +1,17 @@
+# config/initializers/products_price_ids.rb
+PRODUCTS_PRICE_IDS = [
+  { product: 'Quouch Soli monthly', old_price_ids: %w[price_1OtZdXEPEHgNPzy2Z3tzUZTy price_1POI8tEPEHgNPzy2OJfFS1Gd],
+    new_price_id: 'price_1PQTS6EPEHgNPzy2gC9znTnz' },
+  { product: 'Quouch Soli yearly', old_price_ids: %w[price_1OtZg9EPEHgNPzy2vvcMTICD price_1POI7TEPEHgNPzy2VdkEurmH],
+    new_price_id: 'price_1PQTR5EPEHgNPzy2AxGid5tS' },
+  { product: 'Quouch Member monthly',
+    old_price_ids: %w[price_1OtZkQEPEHgNPzy2hahIQGzs price_1POI6DEPEHgNPzy2He6TJS3y],
+    new_price_id: 'price_1PQTOiEPEHgNPzy2G4IohCIa' },
+  { product: 'Quouch Member yearly',
+    old_price_ids: %w[price_1OtZjjEPEHgNPzy2JMjJlzwv price_1POI4fEPEHgNPzy2p8oeGZVx],
+    new_price_id: 'price_1PQTPvEPEHgNPzy2mnBOHMIy' },
+  { product: 'Quouch Supporter monthly', old_price_ids: %w[price_1OtZkqEPEHgNPzy2DI6MurbE price_1POI35EPEHgNPzy2OtlgDYzR],
+    new_price_id: 'price_1PQEckEPEHgNPzy2NSGK31B2' },
+  { product: 'Quouch Supporter yearly', old_price_ids: %w[price_1OtZn1EPEHgNPzy2IfuZJq3e price_1POI1KEPEHgNPzy2opcY0aYU],
+    new_price_id: 'price_1PQTLOEPEHgNPzy2UzAN8Ij3' }
+]
