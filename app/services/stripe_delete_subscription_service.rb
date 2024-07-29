c5b6559e6db9461c6908624c5d197bf4c3d1acc9
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2024-07-29 16:15:23 +0200

    expand stripe delete subscription service to create new subscription with new price id if subscription references old price id, add price id global variable as stripe config file

--- app/services/stripe_delete_subscription_service.rb
+++ app/services/stripe_delete_subscription_service.rb
@@ -1,6 +1,58 @@
 class StripeDeleteSubscriptionService
   def call(event)
-    subscription = event.data.object.id
-    Subscription.find_by(stripe_id: subscription).destroy
+    stripe_id = event.data.object.id
+    subscription = Subscription.find_by(stripe_id:)
+    user_id = User.find(subscription.user_id)
+    return unless subscription
+
+    stripe_subscription = Stripe::Subscription.retrieve(stripe_id)
+    stripe_price_id = stripe_subscription.items.data.first.price.id
+    cancel_at_period_end = stripe_subscription.cancel_at_period_end
+
+    matching_price_config = find_matching_price_config(stripe_price_id)
+
+    if matching_price_config
+      unless cancel_at_period_end
+        new_price_id = matching_price_config[:new_price_id]
+
+        user_payment_method_id = fetch_user_payment_method(user_id)
+
+        create_new_subscription(user_id, new_price_id, user_payment_method_id)
+      end
+    else
+      Rails.logger.error "No matching price configuration found for stripe_price_id #{stripe_price_id}"
+    end
+
+    subscription.destroy
+  end
+
+  private
+
+  def find_matching_price_config(stripe_price_id)
+    PRODUCTS_PRICE_IDS.each do |price_config|
+      return price_config if price_config[:old_price_ids].include?(stripe_price_id)
+    end
+    nil
+  end
+
+  def fetch_user_payment_method(user_id)
+    stripe_customer_id = find_stripe_customer_id(user_id)
+    customer = Stripe::Customer.retrieve(stripe_customer_id)
+    customer.invoice_settings.default_payment_method
+  end
+
+  def create_new_subscription(user_id, new_price_id, payment_method_id)
+    stripe_customer_id = find_stripe_customer_id(user_id)
+
+    Stripe::Subscription.create(
+      customer: stripe_customer_id,
+      items: [{ price: new_price_id }],
+      default_payment_method: payment_method_id,
+      expand: ['latest_invoice.payment_intent']
+    )
+  end
+
+  def find_stripe_customer_id(user_id)
+    User.find(user_id).stripe_id
   end
 end
