84e0de531216b6ef701f2043f1755c544ff20b9d
Lisbeth Purrucker <71633567+lisbethpurrucker@users.noreply.github.com>
2024-08-14 20:13:54 +0200

    Update system_sign_in_helper.rb
    Fixed a typo.

--- test/helpers/system_sign_in_helper.rb
+++ test/helpers/system_sign_in_helper.rb
@@ -4,7 +4,7 @@ module SystemSignInHelper
     visit new_user_session_path
     fill_in 'Email Address', with: user.email
     fill_in 'Password', with: user.password
-    # There are two log in buttons, one if a link and the other one is an input.
+    # There are two log in buttons, one is a link and the other one is an input.
     # We want to click on the input one.
     find('input[type="submit"]').click
   end
