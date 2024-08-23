4f7387adaccfdc0a6730e73ad1757d2aa15d91de
Ross Bermudez <berm.ross@gmail.com>
2024-08-23 19:18:59 +0200

    fix: strip user invite code to prevent issues (#115)
    * fix: strip user invite code to prevent issues
    
    * do not redirect with wrong invite code

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -4,8 +4,8 @@ class InvitesController < ApplicationController
   def invite_code_form; end
 
   def validate_invite_code
-    invite_code = params[:invite][:invite_code]
-    if User.exists?(invite_code: invite_code.strip) || User.exists?(invite_code: invite_code.strip.downcase)
+    invite_code = params[:invite][:invite_code].strip.downcase
+    if User.exists?(invite_code: invite_code)
       redirect_to new_user_registration_path(invite_code:)
     else
       flash[:alert] = 'Invite code not valid. Try again or contact the Quouch team'
