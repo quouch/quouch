3602b22bfe88a6da0175d2640aa17fa6ee1a18d5
papotte <berm.ross@gmail.com>
2024-08-26 13:36:39 +0200

    Create invite_code_helper and add code check step

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,20 +1,34 @@
 class InvitesController < ApplicationController
+  include InviteCodeHelper
   skip_before_action :authenticate_user!, only: %i[invite_code_form validate_invite_code]
+
+  ERROR_MESSAGES = {
+    invalid_code: 'This does not look like a valid code. Please make sure you have the correct code or contact the Quouch Team',
+    invite_code_not_found: 'Invite code not found. Try again or contact the Quouch team',
+    default: 'Something went wrong. Please try again or contact the Quouch team'
+  }.freeze
 
   def invite_code_form; end
 
   def validate_invite_code
     invite_code = params[:invite][:invite_code].strip.downcase
-    begin
-      user = User.find_by!(invite_code: invite_code)
-      redirect_to new_user_registration_path(invite_code: invite_code) if user
-    rescue ActiveRecord::RecordNotFound => exception
-      flash[:alert] = 'Invite code not valid. Try again or contact the Quouch team'
-      Sentry.capture_exception(exception)
-      Sentry.capture_message("No user found with invite code: #{invite_code}")
-
+    if valid_syntax?(invite_code)
+      user = User.find_by!(invite_code:)
+      redirect_to new_user_registration_path(invite_code:) if user
+    else
+      flash[:alert] = ERROR_MESSAGES[:invalid_code]
       render :invite_code_form, status: :unprocessable_entity
+      nil
     end
+  rescue ActiveRecord::RecordNotFound => e
+    flash[:alert] = ERROR_MESSAGES[:invite_code_not_found]
+    Sentry.capture_exception(e)
+    Sentry.capture_message("No user found with invite code: #{invite_code}", level: 'info')
+
+    render :invite_code_form, status: :unprocessable_entity
+  rescue StandardError
+    flash[:alert] = ERROR_MESSAGES[:default]
+    render :invite_code_form, status: :unprocessable_entity
   end
 
   def invite_friend
