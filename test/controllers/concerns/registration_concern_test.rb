801a38a6ca6a4bdc6d65d7b9669fe343831791fe
Ross Bermudez <berm.ross@gmail.com>
2024-08-26 14:36:38 +0200

    fix: add logging for country issue (#118)

--- test/controllers/concerns/registration_concern_test.rb
+++ test/controllers/concerns/registration_concern_test.rb
@@ -21,6 +21,27 @@ class RegistrationConcernTest < ActiveSupport::TestCase
     end
   end
 
+  test 'should throw an error if the country is not found' do
+    params[:user] = {
+      country: 'XX'
+    }
+
+    assert_raise(ArgumentError) { beautify_country }
+  end
+
+  test 'should throw an error if the country cannot be translated' do
+    issue_countries_in_db = %w[DE NL FI US AT FR NO]
+
+    issue_countries_in_db.each do |country_code|
+      params[:user] = {
+        country: country_code
+      }
+
+      # Assert that no errors are thrown for the countries in the list
+      assert_nothing_raised { beautify_country }
+    end
+  end
+
   test 'should find the user ID by invite code' do
     other_user = FactoryBot.create(:user)
     invite_code = other_user.invite_code
