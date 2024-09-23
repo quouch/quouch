6b62fd88550b018a285bc0186005396d1011a559
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2024-09-23 12:58:26 +0200

    add retry statement, add sentry logging

--- app/jobs/bookings_job.rb
+++ app/jobs/bookings_job.rb
@@ -1,9 +1,15 @@
 class BookingsJob < ApplicationJob
   queue_as :default
 
+  # Retry on SMTP Timeout errors with a wait period and limited attempts
+  retry_on Net::SMTPFatalError, Net::ReadTimeout, wait: ->(attempt) { 5.seconds * (2**attempt) }, attempts: 3
+
   def perform
     complete_bookings
     remind_bookings
+  rescue StandardError => e
+    Sentry.capture_exception(e)
+    raise e
   end
 
   def complete_bookings
@@ -12,6 +18,9 @@ class BookingsJob < ApplicationJob
 
     send_completed_emails(completed_bookings)
     update_status(completed_bookings, 4)
+  rescue StandardError => e
+    Sentry.capture_exception(e)
+    raise e
   end
 
   def remind_bookings
@@ -29,6 +38,8 @@ class BookingsJob < ApplicationJob
 
     pending_future_bookings = pending_future_fixed_bookings.or(pending_future_flexible_bookings)
 
+    Sentry.capture_message("Found #{pending_future_bookings.size} pending future bookings")
+
     pending_past_bookings = Booking.where(status: 0).where('start_date < ?', Date.today)
                                    .or(Booking.where(status: 0).where(start_date: nil, flexible: true)
                                    .where('booking_date < ?', one_month_ago))
@@ -37,6 +48,9 @@ class BookingsJob < ApplicationJob
     return if pending_future_bookings.empty?
 
     send_reminder_emails(pending_future_bookings)
+  rescue StandardError => e
+    Sentry.capture_exception(e)
+    raise e
   end
 
   def update_status(bookings, status)
