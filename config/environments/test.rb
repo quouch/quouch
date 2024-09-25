29a011c0cfd9954d7ad5dcf859db5b133c5fc29a
Lisbeth Purrucker <71633567+lisbethpurrucker@users.noreply.github.com>
2024-09-25 15:33:26 +0200

    add sidekiq (#176)
    * add sidekiq
    
    * disable sidekiq in test environment
    
    * switch from inline to test
    
    * add perform enqueued jobs before email delivery size increment check, include sidekiq in bookings job
    
    * remove sidekiq from job
    
    * remove perform enqueued jobs, exchange delivery size check by enqueued emails check
    
    * add test helper to bookings concern test
    
    * replace active job test helper with action mailer test helper
    
    * fix: set back to pending as expected status

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -64,9 +64,13 @@ Rails.application.configure do
   # Tell Active Support which deprecation messages to disallow.
   config.active_support.disallowed_deprecation_warnings = []
 
+  # Disable Sidekiq in test environment.
+  config.active_job.queue_adapter = :test
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+  #
 end
