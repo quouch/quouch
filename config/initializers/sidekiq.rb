80e4709e883037f5ae610ab1779eabbbed88bdf2
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2024-09-26 18:35:57 +0200

    format, remove duplicate initializer

--- config/initializers/sidekiq.rb
+++
@@ -1,9 +0,0 @@
-rediscloud_url = ENV.fetch('REDISCLOUD_URL', nil)
-
-Sidekiq.configure_server do |config|
-  config.redis = { url: rediscloud_url, size: 12 }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: rediscloud_url, size: 3 }
-end
