6e0cff02fa9f94ae48a6b6071a78b6a78156433c
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2024-09-26 15:29:59 +0200

    remove redis protocol 2

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,11 +1,9 @@
 rediscloud_url = ENV.fetch('REDISCLOUD_URL', nil)
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: rediscloud_url, size: 12,
-                   protocol: 2 }
+  config.redis = { url: rediscloud_url, size: 12 }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: rediscloud_url, size: 3,
-                   protocol: 2 }
+  config.redis = { url: rediscloud_url, size: 3 }
 end
