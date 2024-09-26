666d40bc06fb0687559babae5ef8e0b9432ab945
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2024-09-26 14:23:48 +0200

    specifiy protocol for redis sidekiq connection

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,9 +1,11 @@
 rediscloud_url = ENV.fetch('REDISCLOUD_URL', nil)
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: rediscloud_url, size: 12 }
+  config.redis = { url: rediscloud_url, size: 12,
+                   protocol: 2 }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: rediscloud_url, size: 3 }
+  config.redis = { url: rediscloud_url, size: 3,
+                   protocol: 2 }
 end
