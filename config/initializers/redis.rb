87af21ea2e2de803aa3332a09d15fea1b6d877ca
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2024-09-27 12:41:16 +0200

    correct credential syntax

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,11 +1,11 @@
-rediscloud_url = Rails.application.credentials.redis[:url]
+rediscloud_url = Rails.application.credentials.dig(:redis, :url)
 
 Sidekiq.configure_server do |config|
   config.redis = { url: rediscloud_url, size: 12,
-                   password: Rails.application.credentials.redis[:password] }
+                   password: Rails.application.credentials.dig(:redis, :password) }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: rediscloud_url, size: 3,
-                   password: Rails.application.credentials.redis[:password] }
+                   password: Rails.application.credentials.dig(:redis, :password) }
 end
