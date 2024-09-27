9b4e2dad5244e438f3bfb525580ac36d0e80f244
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2024-09-27 11:55:27 +0200

    set redis variables as credentials

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,9 +1,11 @@
-rediscloud_url = ENV.fetch('REDISCLOUD_URL', nil)
+rediscloud_url = Rails.application.credentials.redis[:url]
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: rediscloud_url, size: 12 }
+  config.redis = { url: rediscloud_url, size: 12,
+                   password: Rails.application.credentials.redis[:password] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: rediscloud_url, size: 3 }
+  config.redis = { url: rediscloud_url, size: 3,
+                   password: Rails.application.credentials.redis[:password] }
 end
