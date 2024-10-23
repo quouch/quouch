bfb9efc9594dc3d7d5c37a77e3512b61075a6a44
Lisbeth Purrucker <lisbethpurrucker@gmail.com>
2024-10-23 17:30:27 +0100

    prettifier

--- app/javascript/controllers/plans_toggle_controller.js
+++ app/javascript/controllers/plans_toggle_controller.js
@@ -2,31 +2,31 @@ import { Controller } from '@hotwired/stimulus'
 
 // Connects to data-controller="plans-toggle"
 export default class extends Controller {
-    static targets = ['toggle', 'month', 'sixmonths', 'year']
-    static values = {
-        interval: String
-    }
+	static targets = ['toggle', 'month', 'sixmonths', 'year']
+	static values = {
+		interval: String,
+	}
 
-    connect() {
-        if (this.intervalValue) {
-            this.switch({ target: { value: this.intervalValue } });
-        }
-    }
+	connect() {
+		if (this.intervalValue) {
+			this.switch({ target: { value: this.intervalValue } })
+		}
+	}
 
-    switch(event) {
-        const selectedPlan = event.target.value;
+	switch(event) {
+		const selectedPlan = event.target.value
 
-        // Hide all targets initially
-        this.monthTarget.classList.add('display-none');
-        this.sixmonthsTarget.classList.add('display-none');
-        this.yearTarget.classList.add('display-none');
+		// Hide all targets initially
+		this.monthTarget.classList.add('display-none')
+		this.sixmonthsTarget.classList.add('display-none')
+		this.yearTarget.classList.add('display-none')
 
-        if (selectedPlan === 'month') {
-            this.monthTarget.classList.remove('display-none');
-        } else if (selectedPlan === 'sixmonths') {
-            this.sixmonthsTarget.classList.remove('display-none');
-        } else if (selectedPlan === 'year') {
-            this.yearTarget.classList.remove('display-none');
-        }
-    }
+		if (selectedPlan === 'month') {
+			this.monthTarget.classList.remove('display-none')
+		} else if (selectedPlan === 'sixmonths') {
+			this.sixmonthsTarget.classList.remove('display-none')
+		} else if (selectedPlan === 'year') {
+			this.yearTarget.classList.remove('display-none')
+		}
+	}
 }
